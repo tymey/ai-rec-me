d69552d4c78c89e6e7693101d86f8ece295fce91
tymey <tyler.meyer.a@gmail.com>
2025-05-14 09:14:06 -0500

    Create evaluating script for model

---
+++ src/models/evaluate.py
@@ -0,0 +1,28 @@
+"""
+src/models/evaluate.py
+-----------------------
+Evaluate a trained recommendation model.
+"""
+
+import argparse
+import pickle
+from surprise import accuracy
+
+def evaluate(model_file: str, test_file: str):
+    # Load
+    with open(model_file, "rb") as f:
+        algo = pickle.load(f)
+    with open(test_file, "rb") as f:
+        testset = pickle.load(f)
+    # Evaluate
+    preds = algo.test(testset)
+    rmse = accuracy.rmse(preds, verbose=True)
+    mae  = accuracy.mae(preds, verbose=True)
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("--model-file", required=True)
+    p.add_argument("--test-file",  required=True)
+    args = p.parse_args()
+    evaluate(args.model_file, args.test_file)
+
